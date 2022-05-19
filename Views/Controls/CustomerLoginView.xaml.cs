ea035512f2d72d496ac8acb2cbffa1a183b77a2c
thepicture <85333076+thepicture@users.noreply.github.com>
2022-05-19 13:53:31 +0500

    feat: add focused and default elements

--- Views/Controls/CustomerLoginView.xaml.cs
+++ Views/Controls/CustomerLoginView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LawOfficeDesktopApp.Views.Controls
 {
@@ -23,6 +11,11 @@ namespace LawOfficeDesktopApp.Views.Controls
         public CustomerLoginView()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Keyboard.Focus(LoginBox);
         }
     }
 }
--- Views/Controls/EmployeeLoginView.xaml.cs
+++ Views/Controls/EmployeeLoginView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LawOfficeDesktopApp.Views.Controls
 {
@@ -23,6 +12,19 @@ namespace LawOfficeDesktopApp.Views.Controls
         public EmployeeLoginView()
         {
             InitializeComponent();
+        }
+
+        private void OnVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible)
+            {
+                LoginBox.Focus();
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(LoginBox);
         }
     }
 }
